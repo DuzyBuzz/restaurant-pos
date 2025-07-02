c1c4e6e71943dbdea5d80c1cf32c8bdde8a4cfd6
DuzyBuzz <wenweng003@gmail.com>
2025-07-02 19:46:16 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'flexi-dev',
+  webDir: 'www'
+};
+
+export default config;
