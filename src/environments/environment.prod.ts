5cebc83cc13eecbcd04e6f2b26a92d4178235a5e
DuzyBuzz <wenweng003@gmail.com>
2025-07-04 23:44:55 +0800

    update vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://neojdbtzuotqskjwkhjp.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lb2pkYnR6dW90cXNrandraGpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2MzQyMzQsImV4cCI6MjA2NzIxMDIzNH0.MWcBePmU0OBNTfNlllZLsxZjVNpLtnkCnD41zCY7M_k'
 };
